887eb86318d0b4a12622fb4037f28328947df26c
Devin East <devineast1987@gmail.com>
2026-09-09 07:46:58 -0500

    Update site to production domain

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://kam-website-flame.vercel.app"),
+  metadataBase: new URL("https://www.kansasarchmetals.com"),
 
   title: {
     default:
--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://kam-website-flame.vercel.app";
+  const baseUrl = "https://www.kansasarchmetals.com";
 
   return {
     rules: {
